6897c77bb6936082321feaea61c043bb95e8452d
Anton Nikitin <antonnik00@mail.ru>
2026-07-25 23:10:20 +0300

    ﻿Initial commit
    Source, config, and content. HorrorBasement is local-only (exceeds GitHub 2 GiB push limit).

---
+++ Source/Fogbound/InventoryComponent.cpp
@@ -0,0 +1,77 @@
+﻿#include "InventoryComponent.h"
+#include "UsableItem.h"
+#include "Net/UnrealNetwork.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+
+// Конструктор
+UInventoryComponent::UInventoryComponent()
+{
+    SetIsReplicatedByDefault(true);
+
+    Inventory.SetNum(4);
+}
+
+void UInventoryComponent::AddItem(AUsableItem* NewItem)
+{
+    if (!NewItem || bFull) return;
+
+    FInventoryItem Item;
+    Item.ItemClass = NewItem->GetClass();
+    Item.ItemName = NewItem->ItemName;
+    Item.Icon = NewItem->Icon;
+
+    for (int32 i = 0; i < Inventory.Num(); ++i)
+    {
+        if (Inventory[i].ItemClass == nullptr)
+        {
+            Inventory[i] = Item;
+
+            OnItemAdded.Broadcast(Item.Icon, i);
+            
+            break;
+        }
+    }
+
+    NewItem->Destroy();
+
+    EquippedSlots++;
+    if (EquippedSlots == 4)
+        bFull = true;
+}
+
+void UInventoryComponent::RemoveItem()
+{
+    if (!Inventory.IsValidIndex(SelectedSlot)) return;
+    
+    Inventory[SelectedSlot] = FInventoryItem();
+
+    OnItemRemoved.Broadcast(SelectedSlot);
+    
+    EquippedSlots--;
+
+    SelectedSlot = -1;
+
+    if (bFull && EquippedSlots < 4)
+        bFull = false;
+}
+
+void UInventoryComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(UInventoryComponent, SelectedSlot);
+}
+
+void UInventoryComponent::SetSelectedSlot()
+{
+    const bool bIsSelected = SelectedSlot != -1;
+    
+    OnSelectedSlotChanged.Broadcast(SelectedSlot, bIsSelected);
+}
+
+void UInventoryComponent::OnRep_SelectedSlot() const
+{
+    const bool bIsSelected = SelectedSlot != -1;
+    OnSelectedSlotChanged.Broadcast(SelectedSlot, bIsSelected);
+}
---
+++ Source/Fogbound/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class FOGBOUND_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
