ac704691fab184e6d21715899cbcc8a010b6144c
Anton Nikitin <antonnik00@mail.ru>
2026-07-26 01:00:09 +0300

    update inventory replication on client

--- Source/Fogbound/InventoryComponent.cpp
+++ Source/Fogbound/InventoryComponent.cpp
@@ -4,74 +4,148 @@
 #include "GameFramework/Actor.h"
 #include "Engine/World.h"
 
-// Конструктор
 UInventoryComponent::UInventoryComponent()
 {
-    SetIsReplicatedByDefault(true);
+	SetIsReplicatedByDefault(true);
 
-    Inventory.SetNum(4);
+	Inventory.SetNum(4);
+	PreviousInventory.SetNum(4);
+}
+
+bool UInventoryComponent::OwnerHasAuthority() const
+{
+	const AActor* Owner = GetOwner();
+	return Owner && Owner->HasAuthority();
+}
+
+void UInventoryComponent::NotifyInventoryDelta()
+{
+	const int32 Num = FMath::Max(Inventory.Num(), PreviousInventory.Num());
+	for (int32 i = 0; i < Num; ++i)
+	{
+		const bool bHadItem = PreviousInventory.IsValidIndex(i) && PreviousInventory[i].ItemClass != nullptr;
+		const bool bHasItem = Inventory.IsValidIndex(i) && Inventory[i].ItemClass != nullptr;
+
+		if (!bHadItem && bHasItem)
+		{
+			OnItemAdded.Broadcast(Inventory[i].Icon, i);
+		}
+		else if (bHadItem && !bHasItem)
+		{
+			OnItemRemoved.Broadcast(i);
+		}
+		else if (bHadItem && bHasItem && PreviousInventory[i].ItemClass != Inventory[i].ItemClass)
+		{
+			OnItemRemoved.Broadcast(i);
+			OnItemAdded.Broadcast(Inventory[i].Icon, i);
+		}
+	}
+
+	PreviousInventory = Inventory;
 }
 
 void UInventoryComponent::AddItem(AUsableItem* NewItem)
 {
-    if (!NewItem || bFull) return;
+	if (!OwnerHasAuthority() || !NewItem || bFull)
+	{
+		return;
+	}
 
-    FInventoryItem Item;
-    Item.ItemClass = NewItem->GetClass();
-    Item.ItemName = NewItem->ItemName;
-    Item.Icon = NewItem->Icon;
+	FInventoryItem Item;
+	Item.ItemClass = NewItem->GetClass();
+	Item.ItemName = NewItem->ItemName;
+	Item.Icon = NewItem->Icon;
 
-    for (int32 i = 0; i < Inventory.Num(); ++i)
-    {
-        if (Inventory[i].ItemClass == nullptr)
-        {
-            Inventory[i] = Item;
+	bool bAdded = false;
+	for (int32 i = 0; i < Inventory.Num(); ++i)
+	{
+		if (Inventory[i].ItemClass == nullptr)
+		{
+			Inventory[i] = Item;
+			bAdded = true;
+			break;
+		}
+	}
 
-            OnItemAdded.Broadcast(Item.Icon, i);
-            
-            break;
-        }
-    }
+	if (!bAdded)
+	{
+		return;
+	}
 
-    NewItem->Destroy();
+	NewItem->Destroy();
 
-    EquippedSlots++;
-    if (EquippedSlots == 4)
-        bFull = true;
+	EquippedSlots++;
+	if (EquippedSlots == 4)
+	{
+		bFull = true;
+	}
+
+	NotifyInventoryDelta();
 }
 
 void UInventoryComponent::RemoveItem()
 {
-    if (!Inventory.IsValidIndex(SelectedSlot)) return;
-    
-    Inventory[SelectedSlot] = FInventoryItem();
+	if (!OwnerHasAuthority())
+	{
+		return;
+	}
 
-    OnItemRemoved.Broadcast(SelectedSlot);
-    
-    EquippedSlots--;
+	if (!Inventory.IsValidIndex(SelectedSlot) || Inventory[SelectedSlot].ItemClass == nullptr)
+	{
+		return;
+	}
 
-    SelectedSlot = -1;
+	Inventory[SelectedSlot] = FInventoryItem();
 
-    if (bFull && EquippedSlots < 4)
-        bFull = false;
+	EquippedSlots--;
+	SelectedSlot = -1;
+
+	if (bFull && EquippedSlots < 4)
+	{
+		bFull = false;
+	}
+
+	NotifyInventoryDelta();
+}
+
+void UInventoryComponent::TryRemoveItem()
+{
+	if (OwnerHasAuthority())
+	{
+		RemoveItem();
+	}
+	else
+	{
+		Server_RemoveItem();
+	}
+}
+
+void UInventoryComponent::Server_RemoveItem_Implementation()
+{
+	RemoveItem();
 }
 
 void UInventoryComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
-    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
-    DOREPLIFETIME(UInventoryComponent, SelectedSlot);
+	DOREPLIFETIME_CONDITION(UInventoryComponent, Inventory, COND_OwnerOnly);
+	DOREPLIFETIME_CONDITION(UInventoryComponent, SelectedSlot, COND_OwnerOnly);
 }
 
 void UInventoryComponent::SetSelectedSlot()
 {
-    const bool bIsSelected = SelectedSlot != -1;
-    
-    OnSelectedSlotChanged.Broadcast(SelectedSlot, bIsSelected);
+	const bool bIsSelected = SelectedSlot != -1;
+	OnSelectedSlotChanged.Broadcast(SelectedSlot, bIsSelected);
+}
+
+void UInventoryComponent::OnRep_Inventory()
+{
+	NotifyInventoryDelta();
 }
 
 void UInventoryComponent::OnRep_SelectedSlot() const
 {
-    const bool bIsSelected = SelectedSlot != -1;
-    OnSelectedSlotChanged.Broadcast(SelectedSlot, bIsSelected);
+	const bool bIsSelected = SelectedSlot != -1;
+	OnSelectedSlotChanged.Broadcast(SelectedSlot, bIsSelected);
 }
--- Source/Fogbound/InventoryComponent.h
+++ Source/Fogbound/InventoryComponent.h
@@ -7,6 +7,7 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnItemAdded, UTexture2D*, Icon, int32, SlotNumber);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnItemRemoved, int32, SlotNumber);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnSelectedSlotChanged, int32, SlotNumber, bool, bIsSelected);
 
 USTRUCT(BlueprintType)
 struct FInventoryItem
@@ -37,33 +38,49 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "Inventory")
 	FOnItemRemoved OnItemRemoved;
 
-	UPROPERTY(BlueprintReadWrite)
+	UPROPERTY(ReplicatedUsing = OnRep_Inventory, BlueprintReadWrite)
 	TArray<FInventoryItem> Inventory;
 
 	UPROPERTY(ReplicatedUsing = OnRep_SelectedSlot, BlueprintReadWrite)
 	int32 SelectedSlot = -1;
 
-	UFUNCTION(BlueprintCallable)
+	/** Server-only. No-ops without authority. */
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
 	void AddItem(AUsableItem* NewItem);
 
-	UFUNCTION(BlueprintCallable)
+	/** Server-only. Prefer TryRemoveItem from client/UI. */
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
 	void RemoveItem();
+
+	/** Safe from client or server - routes remove to authority. */
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void TryRemoveItem();
 
 	UPROPERTY(BlueprintReadOnly)
 	int EquippedSlots = 0;
 
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnSelectedSlotChanged, int32, SlotNumber, bool, bIsSelected);
-
-	UPROPERTY(BlueprintAssignable)
+	UPROPERTY(BlueprintAssignable, Category = "Inventory")
 	FOnSelectedSlotChanged OnSelectedSlotChanged;
 
 	UFUNCTION(BlueprintCallable)
 	void SetSelectedSlot();
 
+protected:
+	UFUNCTION(Server, Reliable)
+	void Server_RemoveItem();
+
 private:
 	bool bFull = false;
 
+	/** Local mirror for OnRep diffing (not replicated). */
+	TArray<FInventoryItem> PreviousInventory;
+
+	bool OwnerHasAuthority() const;
+	void NotifyInventoryDelta();
+
+	UFUNCTION()
+	void OnRep_Inventory();
+
 	UFUNCTION()
 	void OnRep_SelectedSlot() const;
-	
 };
